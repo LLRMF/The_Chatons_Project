b9039f53672726bd88dcca59e54cc62db3030061
R4ja34 <raja.lalaina@gmail.com>
2023-11-29 11:10:23 +0100

    add item and remove item to cart fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,25 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "static_pages#home"
-  resources :items, only: [:index, :show]
   devise_for :users
-
-  resources :items, only: [:index, :show] do
-    member do
-      post 'add_to_cart/:item_id', action: :add_to_cart, as: :add_to_cart
-    end
+  resources :users
+  
+  # Static pages
+  get "about", to: "static_pages#about"
+  get "contact", to: "static_pages#contact"
+  
+  resources :items do
+    post 'add_to_cart', on: :member
   end
 
-
-  resources :carts, only: [:index, :create, :show] do
-    member do
-      delete 'remove_item/:item_id', action: :remove_item, as: :remove_item
-    end
+  resources :carts do
+    delete 'remove_item/:item_id', to: 'carts#remove_item', on: :member, as: 'remove_item'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", :as => :rails_health_check
-
-  # root "posts#index"
-
-  # Static pages
-  get "about", to: "static_pages#about"
-  get "contact", to: "static_pages#contact"
-
-
-
-  resources :users
-
-
 
   scope '/checkout' do
     post 'create', to: 'checkout#create', as: 'checkout_create'
