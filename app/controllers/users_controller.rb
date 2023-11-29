456bde69e2cc6bb3031b2017ac0c7ddeb2c8edf2
mathieu.amacher <amacher_mathieu@hotmail.com>
2023-11-29 12:35:42 +0100

    modification apporter à user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,12 @@
 class UsersController < ApplicationController
   
   
-  def index
-    @users = User.all
-  end
-
   def show
-    @user = User.find(params[:id])
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.create(email: params['email'], encrypted_password: params['password'])
-    @event.avatar.attach(params[:avatar])
+    @user = current_user   
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
-  def update
-    @user = User.find(params[:id])
-    @user.update
-  end
-
-  def destroy
-    @event = Event.find(params[:id])
-    @user.destroy
-  end
 end
