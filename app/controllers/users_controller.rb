29826bb1a2fa1955216b19e6aa361aa10c47d6f1
R4ja34 <raja.lalaina@gmail.com>
2023-11-30 11:28:06 +0100

    avec view orders in user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,27 @@ class UsersController < ApplicationController
   
   
   def show
-    @user = current_user   
+    @user = current_user  
+    @orders = @user.orders
   end
 
   def edit
     @user = current_user
   end
 
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :description, :email, :password, :password_confirmation, :current_password)
+  end
+  
 end
