acc2ad805cc6d604a445bcf48a52fa91e2675a5b
R4ja34 <raja.lalaina@gmail.com>
2023-11-30 02:32:51 +0100

    fix parcours cart order checkout

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,38 +1,44 @@
 class CheckoutController < ApplicationController
-    def create
-      @total = params[:total].to_d
-      @event_id = params[:event_id]
-      @session = Stripe::Checkout::Session.create(
-        payment_method_types: ['card'],
-        line_items: [
-          {
-            price_data: {
-              currency: 'eur',
-              unit_amount: (@total*100).to_i,
-              product_data: {
-                name: 'Rails Stripe Checkout',
-              },
-            },
-            quantity: 1
+  def create
+    @total = params[:total].to_d
+    @order_id = params[:order_id]
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price_data: {
+            currency: 'eur',
+            unit_amount: (@total * 100).to_i,
+            product_data: {
+              name: 'Rails Stripe Checkout'
+            }
           },
-          metadata: {
-            event_id: @event_id
-          },
-        ],
-        mode: 'payment',
-        success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
-        cancel_url: checkout_cancel_url
-      )
-      redirect_to @session.url, allow_other_host: true
-    end
-  
-    def success
-      @session = Stripe::Checkout::Session.retrieve(params[:session_id])
-      @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
-      @event_id = @session.metadata.event_id
-    end
-  
-    def cancel
-    end
+          quantity: 1
+        }
+      ],
+      metadata: {
+        order_id: @order_id
+      },
+      mode: 'payment',
+      success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: checkout_cancel_url
+    )
+    redirect_to @session.url, allow_other_host: true
   end
-  
+
+  def success
+    @session = Stripe::Checkout::Session.retrieve(params[:session_id])
+    @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+    clear_user_cart if @payment_intent.status == 'succeeded'
+  end
+
+  def cancel
+  end
+
+  private
+
+  def clear_user_cart
+    current_user.cart.cart_items.destroy_all
+  end
+
+end
