722e0a32be2f3bb4bee8aa3b163e27a9369fe71b
R4ja34 <raja.lalaina@gmail.com>
2023-11-30 09:46:21 +0100

    fix order

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,7 +1,7 @@
 class CheckoutController < ApplicationController
   def create
     @total = params[:total].to_d
-    @order_id = params[:order_id]
+    @cart_id = params[:cart_id]
     @session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: [
@@ -20,6 +20,7 @@ class CheckoutController < ApplicationController
         order_id: @order_id
       },
       mode: 'payment',
+      customer_email: current_user.email,
       success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
       cancel_url: checkout_cancel_url
     )
@@ -29,6 +30,9 @@ class CheckoutController < ApplicationController
   def success
     @session = Stripe::Checkout::Session.retrieve(params[:session_id])
     @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+    @user = current_user
+    @order = Order.new(user_id: current_user.id)
+    create_order_items(current_user.cart.cart_items) if @order.save
     clear_user_cart if @payment_intent.status == 'succeeded'
   end
 
@@ -36,6 +40,17 @@ class CheckoutController < ApplicationController
   end
 
   private
+
+
+  def create_order_items(cart_items)
+    cart_items.each do |cart_item|
+      order_item = @order.order_items.build(
+        item_id: cart_item.item_id,
+        quantity: cart_item.quantity
+      )
+      order_item.save
+    end
+  end
 
   def clear_user_cart
     current_user.cart.cart_items.destroy_all
